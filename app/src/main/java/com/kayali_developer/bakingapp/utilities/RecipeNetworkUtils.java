636387e1966c838873f0f4496726cd163c5959b4
Abdulkadir Kayali <abdulkadir.kayali@swiss-commerce.ch>
2018-09-17 14:46:48 +0200

    Ready for submission

--- app/src/main/java/com/kayali_developer/bakingapp/utilities/RecipeNetworkUtils.java
+++ app/src/main/java/com/kayali_developer/bakingapp/utilities/RecipeNetworkUtils.java
@@ -4,13 +4,6 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
-
 public class RecipeNetworkUtils {
     // Constructor
     public RecipeNetworkUtils() {
@@ -23,34 +16,5 @@ public class RecipeNetworkUtils {
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected());
-    }
-
-    // Casting String Url to Url Object
-    public static URL createUrl(String stringUrl) {
-        URL url = null;
-        try {
-            url = new URL(stringUrl);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return url;
-    }
-
-    // Open HttpURLConnection and fetch response using Scanner
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            InputStream in = urlConnection.getInputStream();
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
-            urlConnection.disconnect();
-        }
     }
 }
--- app/src/main/java/com/kayali_developer/bakingapp/view_model/MainViewModel.java
+++ app/src/main/java/com/kayali_developer/bakingapp/view_model/MainViewModel.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.widget.Toast;
 
@@ -12,7 +11,6 @@ import com.kayali_developer.bakingapp.R;
 import com.kayali_developer.bakingapp.api.ApiCallback;
 import com.kayali_developer.bakingapp.api.ApiManager;
 import com.kayali_developer.bakingapp.data.model.Recipe;
-import com.kayali_developer.bakingapp.utilities.RecipeJSONUtils;
 import com.kayali_developer.bakingapp.utilities.RecipeNetworkUtils;
 
 import java.util.List;
@@ -37,13 +35,7 @@ public class MainViewModel extends AndroidViewModel {
     }
 
     private void loadRecipes() {
-        /*
-        if (RecipeNetworkUtils.isConnected(mContext)) {
-            RecipeAsyncTask task = new RecipeAsyncTask();
-            task.execute(AppConstants.REQUEST_URL);
 
-        }
-*/
         if (RecipeNetworkUtils.isConnected(mContext)) {
             ApiManager.getInstance().getRecipes(new ApiCallback<List<Recipe>>() {
                 @Override
@@ -54,6 +46,7 @@ public class MainViewModel extends AndroidViewModel {
                         Toast.makeText(mContext, mContext.getResources().getString(R.string.no_data_to_display), Toast.LENGTH_SHORT).show();
                     }
                 }
+
                 @Override
                 public void onCancel() {
                     Toast.makeText(mContext, mContext.getResources().getString(R.string.loading_canceled), Toast.LENGTH_SHORT).show();
@@ -63,30 +56,7 @@ public class MainViewModel extends AndroidViewModel {
             Toast.makeText(mContext, mContext.getResources().getString(R.string.no_internet), Toast.LENGTH_SHORT).show();
         }
     }
-/*
-    // AsyncTask class to load Recipes data on Background
-    private class RecipeAsyncTask extends AsyncTask<String, Void, List<Recipe>> {
-        @Override
-        protected List<Recipe> doInBackground(String... urls) {
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-            return RecipeJSONUtils.extractRecipesData(urls[0]);
-        }
 
-        @Override
-        protected void onPostExecute(List<Recipe> recipes) {
-            if (recipes != null && !recipes.isEmpty()) {
-                setLoadedRecipesToLiveDataObject(recipes);
-            }
-        }
-
-        // Helper method to set retrieved data to MutableLiveData<List<Recipe>> mRecipes
-        private void setLoadedRecipesToLiveDataObject(List<Recipe> recipes) {
-            mRecipes.setValue(recipes);
-        }
-    }
-*/
     // Helper method to set retrieved data to MutableLiveData<List<Recipe>> mRecipes
     private void setLoadedRecipesToLiveDataObject(List<Recipe> recipes) {
         mRecipes.setValue(recipes);
